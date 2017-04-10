cee82721f28601c40c5effb57fe7fc8f9d00e886
Tori Hedden <tori.hedden@gmail.com>
2017-04-09 20:20:38 -0400

    live reload w/ change to any html file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
 gulp.task('sass', function() {
   return gulp.src('src/css/main.scss')
@@ -15,15 +16,11 @@ gulp.task('browserSync', function() {
   browserSync.init({
     server: {
       baseDir: 'src'
-    },
+    }
   })
+  gulp.watch("**/*.html").on("change", reload);
 })
 
 gulp.task('watch', ['browserSync', 'sass'], function() {
   gulp.watch('src/css/*.scss', ['sass']);
-})
-
-gulp.task('build', function() {
-  //build and optimize files for production
-  //pipe to dist directory
 })
